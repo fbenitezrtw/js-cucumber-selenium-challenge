6969f32efcad0c52e04e9c295e7957e934202e8c
Fernando Benitez <48774783+fbenitezrtw@users.noreply.github.com>
2022-10-19 19:36:14 -0300

    Delete pd_challenge directory

--- pd_challenge/page_objects/homePage.js
+++
@@ -1,7 +0,0 @@
-const { By } = require("selenium-webdriver");
-
-const home = function() {
-    this.homeTitle = By.xpath("//h1[@class='styled__StyledText-sc-1pogpev-0 hFnuAt' and contains(text(), 'Employee') or contains(text(), 'Empleado')]");
-}
-
-module.exports = home;
--- pd_challenge/step_definitions/browser.js
+++
@@ -1,5 +0,0 @@
-const { Given } = require('@cucumber/cucumber');
-
-Given(/^I open '(.*)'$/, async function (url) {
-    await this.driver.get(url);
-});
